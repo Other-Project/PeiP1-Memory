63b405deea033c1858d39cf93e6693046496b50b
EvanG <evan_g@orange.fr>
2021-12-10 16:05:58 +0100

    WIP: Fond

--- decor.py
+++ decor.py
@@ -2,7 +2,24 @@
 # Ce module contient le code chargé du dessin de l'interface
 
 import formes
+import turtle
+
 def main(t):
 	"""Le décor dessiné avec la tortue t"""
-	formes.croix(-100, -100, 100, "green", t)
-	formes.croix(-100, 50, 50, "yellow", t)
+	fond(t)
+	etoiles(t)
+
+def fond(t):
+        turtle.bgcolor("midnightblue")
+
+def etoiles(t):
+        taille=25
+        gap=taille*2
+        for y in range(10):
+                padding=0
+                if y % 2 ==0:
+                        padding=gap/2
+                for x in range(10):
+                        formes.etoile(padding+x*gap, y*gap, taille, "gold", t)
+                
+                
